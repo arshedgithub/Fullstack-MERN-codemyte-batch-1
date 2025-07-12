1948f16678f353942394d041c3309697a677071b
arshedgithub <arshedahmedmail@gmail.com>
2025-07-12 22:28:42 +0530

    DTO introduction

---
+++ dto/createUserSchema.js
@@ -0,0 +1,16 @@
+// const createUserSchema = {
+//     name: {
+//         type: String,
+//     },
+//     email: {
+//         type: email
+//     },
+
+//     required: ['name', 'email'],
+//     additionalProperties: false
+// }
+
+// ajv - library for validation
+
+// DTO - Data Transfer Object
+// DAO - Data Access Object
