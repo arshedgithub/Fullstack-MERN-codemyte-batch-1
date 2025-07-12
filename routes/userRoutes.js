ed728a41b8fce141eb85e8ec028aca82224ff529
arshedgithub <arshedahmedmail@gmail.com>
2025-07-12 22:23:14 +0530

    middleware introduction

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,18 +6,37 @@ const userController = require('./../controllers/userController')
 // CRUD Operation 
 
 // GET /api/users => all users 
-router.get('/', userController.getAllUsers );
+router.get('/', middl1, middl2, middl3, userController.getAllUsers );
 
 // GET /api/users/:id => relevant user 
 router.get('/:id', userController.getUserById );
 
 // POST /api/users => new user
-router.post('/', userController.createNewUser );
+router.post('/', validationRquest(createUserSchema), userController.createNewUser );
 
 // PUT /api/users/:id => edit user
 router.put('/:id', userController.updateUser );
 
 // DELETE /api/users/:id => delete user
 router.delete('/:id', userController.deleteUser );
+
+function middl1(req, res, next) {
+    console.log("middleware 1 called.");
+    next();
+}
+
+function authMiddleware(req, res, next) {
+    console.log("middleware 2 called.");
+    if (req.body.user.role == "admin"){
+        next();
+    }
+}
+
+// validationRquest()
+
+function middl3(req, res, next) {
+    console.log("middleware 3 called.");
+    next();
+}
 
 module.exports = router;
