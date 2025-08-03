249b59951aad4f6b4d91a6678570544dd9556eee
arshedgithub <arshedahmedmail@gmail.com>
2025-08-03 23:17:38 +0530

    useeffect hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0);
-  
+  const [message, setMessage] = useState('Hello World!');
+  const [isError, setIsError] = useState(false);
+  // kind of a dynamic object in component memory 
+
   // jsx - javascript xml 
+
+  useEffect(() => {
+    if (count < 0) {
+      setMessage('Negative');
+      setIsError(true);
+    } else {
+      setIsError(false);
+    }
+  }, [count]);
 
   return (
     <>
+
+      {isError ? <h1 style={{ color: 'red', backgroundColor: 'pink', padding: 15, borderRadius: 10 }}>{message}</h1> : <h1>{message}</h1>}
+
       <button onClick={() => setCount((count) => count - 1)}> - </button>
       {count}
       <button onClick={() => setCount((count) => count + 1)}> + </button>
